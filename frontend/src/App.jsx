93cf5495da761fcd9f4a37e305d1975caa12ae94
bonusshyn <shayanisshyn@gmail.com>
2025-05-31 14:28:30 +0700

    NewBook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,8 @@ import LoginPage from "./components/LoginComponent";
 import SignupPage from "./pages/SignUp";
 import { EditAccountPage } from "./pages/EditAccountPage";
 import { BookShelfPage } from "./pages/BookShelfPage";
+import NewBookComponent from "./components/NewBookComponent";
+import NewBookPage from "./pages/NewBookPage";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -27,6 +29,14 @@ const router = createBrowserRouter([
   {
     path: "/bookshelf",
     element: <BookShelfPage />,
+  },
+  {
+    path: "/create-book",
+    element: <NewBookPage />,
+  },
+  {
+    path: "/Story",
+    element: <S />,
   },
 
 ]);
